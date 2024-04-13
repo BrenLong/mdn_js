5fbcf0ef5c37a7df86de90d1026477530e6a996c
BrenLong <brendan.long@shopify.com>
2024-04-13 13:52:56 +0100

    add logo changer

--- scripts/main.js
+++ scripts/main.js
@@ -1,39 +1,11 @@
-const myHeading = document.querySelector("h1");
-myHeading.textContent = "Hello world!";
+const myImage = document.querySelector("img");
 
-const myHeading2 = document.querySelector("#id2");
-myHeading2.textContent = "Hello world x 2!";
-
-/*
-let myVariable = document.querySelector('#id2');
-
-let iceCream = 'chocolate';
-iceCream = 'vanilla';
-
-if(iceCream === 'chocolate') {
-  alert('Yay, I love chocolate ice cream!');
-}
-else {
-  alert('Aww, but chocolate is my favorite...');
-}
-*/
-
-function multiply (num1, num2) {
-  let result = num1*num2;
-  return result;
-}
-
-function divide (num1, num2) {
-  let result = num1/num2;
-  return result;
-}
-
-
-document.querySelector("h1").addEventListener("click", function () {
-  alert("what the....");
-})
-
-
-document.querySelector("#id2").addEventListener("click", () => {
-  alert("quit it");
-})
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/firefox-icon.png") {
+    myImage.setAttribute("src", "images/safari-icon.png");
+  }
+    else {
+      myImage.setAttribute("src", "images/firefox-icon.png");
+    }
+  }
