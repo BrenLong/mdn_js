76d015c786c7a43b1cb766f560791c6e06566faa
BrenLong <brendan.long@shopify.com>
2024-04-13 18:31:37 +0100

    add name prompt

--- scripts/main.js
+++ scripts/main.js
@@ -9,3 +9,21 @@ myImage.onclick = () => {
       myImage.setAttribute("src", "images/firefox-icon.png");
     }
   }
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function getUserName() {
+  const myName = prompt("Enter your name");
+  if (!myName) {
+    getUserName();
+  }
+  else {
+    localStorage.getItem("name", myName);
+    myHeading.textContent = `Mozilla is great, ${myName}`;
+  }
+}
+
+myButton.onclick = () => {
+  getUserName();
+};
