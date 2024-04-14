4c3a3e144edfe171d4699c115f185c5db3744134
BrenLong <brendan.long@shopify.com>
2024-04-14 11:33:37 +0100

    add library functionality

--- scripts/main.js
+++ scripts/main.js
@@ -1,29 +1,34 @@
-const myImage = document.querySelector("img");
+// Add a constructor for book objects
 
-myImage.onclick = () => {
-  const mySrc = myImage.getAttribute("src");
-  if (mySrc === "images/firefox-icon.png") {
-    myImage.setAttribute("src", "images/safari-icon.png");
-  }
-    else {
-      myImage.setAttribute("src", "images/firefox-icon.png");
-    }
-  }
-
-let myButton = document.querySelector("button");
-let myHeading = document.querySelector("h1");
-
-function getUserName() {
-  const myName = prompt("Enter your name");
-  if (!myName) {
-    getUserName();
-  }
-  else {
-    localStorage.getItem("name", myName);
-    myHeading.textContent = `Mozilla is great, ${myName}`;
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.info = function() {
+    return(`${this.title} by ${this.author}, ${this.pages}, ${this.read}`);
   }
 }
 
-myButton.onclick = () => {
-  getUserName();
-};
+// All of your book objects are going to be stored in an array,
+// so add a function to the script (not the constructor)
+// that can take user’s input and store the new book objects into an array.
+
+const library = [];
+
+function getBookDetails() {
+  const title = prompt('Enter the title of the book');
+  const author = prompt('Enter the author of the book');
+  const pages = prompt('Enter the number of pages in the book');
+  const read = prompt('Have you read the book?');
+
+  let book = new Book(title, author, pages, read);
+  library.push(book);
+
+  let anotherBook = prompt('Do you want to add another book? (yes or no)');
+  if (anotherBook === 'yes') {
+    getBookDetails();
+  } else {
+    console.log(library);
+  }
+}
